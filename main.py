c437623317fa1978be153982d5394a8225430da5
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-08-22 14:55:38 +0100

    add info and parse date

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
 # read the csv file
-df = pd.read_csv('Movies_dataset.csv')
+df = pd.read_csv('Movies_dataset.csv',
+                 dayfirst=True,
+                 parse_dates=['release_date'],
+                 )
 
 # print the first 5 rows of the dataframe
 print(df.head())
 
 # print a description of the dataframe
 print(df.describe())
+
+# print information about the dataframe
+print(df.info())
 
 # print a list of the cloumns in the dataframe
 print(df.columns)

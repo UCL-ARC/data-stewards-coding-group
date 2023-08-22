68a8574b52e754ea6bfbd32b2f1b69bbd71b57ad
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-08-22 14:50:28 +0100

    print description

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ import pandas as pd
 # read the csv file
 df = pd.read_csv('Movies_dataset.csv')
 
+# print the first 5 rows of the dataframe
+print(df.head())
+
+# print a description of the dataframe
+print(df.describe())
+
+# print a list of the cloumns in the dataframe
+print(df.columns)
+
 

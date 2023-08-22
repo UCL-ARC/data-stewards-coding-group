9b5b28d7f9cb0e2f55c07cb6f3dd96cf15a04788
Nicholas Owen <nicholas.owen1@gmail.com>
2023-08-22 16:07:12 +0100

    initial import, filter and plot save

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-pass
 
+import pandas as pd
+import seaborn as sns
+
+# import dataframe 
+df = pd.read_csv("Movies_dataset.csv")
+df_2023 = df[df['release_date'].str.contains('2023', na=False)]
+df_2023['release_date'] = pd.to_datetime(df_2023['release_date'], infer_datetime_format=True) # note infer is deprecated
+# Plot the data using Seaborn
+movie_plot = sns.scatterplot(data=df_2023, x="popularity", y="release_date", hue="vote_count")
+
+fig = movie_plot.get_figure()
+fig.savefig("2023_plot.png", dpi=400)

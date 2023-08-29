2c98fb1400758b8803b4487d934a2e87e7522151
katiebuntic <k.buntic@ucl.ac.uk>
2023-08-29 13:49:09 +0100

    updates to README

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-pass
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+movies_df = pd.read_csv('Movies_dataset.csv')
+
+
+# describing the data set
+print(movies_df.head())
+print(movies_df.describe())
